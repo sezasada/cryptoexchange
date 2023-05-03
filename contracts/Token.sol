4f9b3f0ede3ec596055efa058fc0c6a8b3f41de8
Sebastien Zasada <sezasada@icloud.com>
2023-05-03 17:16:45 -0500

    created a transfer function, allowing tokens to be sent between adresses. Created test cases for successful transfers and invalid transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -16,11 +16,40 @@ contract Token {
 
     // Send Tokens
 
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals); // 1,000,000 x 10^18;
         balanceOf[msg.sender] = totalSupply; // msg.sender is an address. msg is a global variable in solidity.
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+
+        // Require that sender has enough tokens to spend
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+        // Deduct tokens from spender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+
+        //Credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        // Emit Event 
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,29 +1,29 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
- 
+
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
 describe("Token", () => {
   // All of the tests
-  let token, accounts, deployer
+  let token, accounts, deployer, receiver;
   beforeEach(async () => {
     // This code will run before each it()
     // Fetching the token from blockchain
     const Token = await ethers.getContractFactory("Token");
     token = await Token.deploy("Sebastien's Token", "SEBZ", 1000000);
 
-    accounts = await ethers.getSigners()
-    deployer = accounts[0]
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    receiver = accounts[1];
   });
 
   describe("Deployment", () => {
-
     const name = "Sebastien's Token";
     const symbol = "SEBZ";
     const decimals = 18;
-    const totalSupply = tokens("1000000")
+    const totalSupply = tokens("1000000");
 
     it("has correct name", async () => {
       // checking that the name is correct
@@ -48,6 +48,47 @@ describe("Token", () => {
       // checking that the total supply is correct
       console.log(deployer.address);
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+    });
+  });
+
+  describe("Sending Token", () => {
+    let amount, transaction, result;
+    describe("Success", () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .transfer(receiver.address, amount);
+        result = await transaction.wait();
+      });
+
+      it("transfers token balances", async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(
+          tokens(999900)
+        );
+        expect(await token.balanceOf(receiver.address)).to.equal(amount);
+      });
+      it("emits a Transfer Event", async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal("Transfer");
+
+        const args = event.args;
+        expect(args.from).to.equal(deployer.address);
+        expect(args.to).to.equal(receiver.address);
+        expect(args.value).to.equal(amount);
+      });
+    });
+    describe("Failure", () => {
+      it("rejects insufficient balances", async () => {
+        // Transfer more tokens than deployer has - 100M
+        const invalidAmount = tokens(100000000);
+        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+      })
+
+      it('rejects invalid recipent', async () => {
+        const amount = tokens(100)
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
     });
   });
 });
