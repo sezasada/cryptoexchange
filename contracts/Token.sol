fb4848474cd13c21a78631994dcf44227f79275b
Sebastien Zasada <sezasada@icloud.com>
2023-05-04 12:32:03 -0500

    finished constructing the ERC-20 token, everything meets the ERC-20 standard, production ready. Wrote tests for it, everything is green and looking good.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -19,15 +19,11 @@ contract Token {
 
     // Send Tokens
 
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 value
-    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     event Approval(
-        address indexed owner, 
-        address indexed spender, 
+        address indexed owner,
+        address indexed spender,
         uint256 value
     );
 
@@ -46,34 +42,53 @@ contract Token {
         address _to,
         uint256 _value
     ) public returns (bool success) {
-
         // Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
-        // Deduct tokens from spender
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
 
-        //Credit tokens to receiver
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        // Emit Event 
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
     }
 
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+
+        balanceOf[_from] = balanceOf[_from] - _value;
+
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
+    }
+
     // Taken directly from the docs
-    function approve(address _spender, uint256 _value) 
-        public 
-        returns(bool success) 
-    {
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
         require(_spender != address(0));
-        
+
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
 
+        // check approval
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        // Reset Allowance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        // spend tokens
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
 }
