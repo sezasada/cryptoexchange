c6f6cf8b83e5c391ac372c7524dea194eb4b98d9
Sebastien Zasada <sezasada@icloud.com>
2023-05-04 17:32:02 -0500

    created withdraw functionality, wrote tests for the withdraw function.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,6 +11,7 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -27,6 +28,21 @@ contract Exchange {
 
         // Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        // Ensure user has enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);
+
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        // Transfer Tokens to user
+        Token (_token).transfer(msg.sender, _amount);
+
+        // Emit an event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
     }
 
     // Check Balances
--- test/Exchange.js
+++ test/Exchange.js
@@ -83,9 +83,87 @@ describe("Exchange", () => {
     });
     describe("Failure", () => {
       it("fails when no tokens are approved", async () => {
-        // Dont approve any tokens before depositing 
+        // Dont approve any tokens before depositing
+        await expect(
+          exchange.connect(user1).depositToken(token1.address, amount)
+        ).to.be.reverted;
+      });
+    });
+  });
+
+  describe("Withdrawing Tokens", () => {
+    let transaction, result;
+    let amount = tokens(10);
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        // Deposit Token before withdraw
+
+
+        // Approve Token
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+
+        // Deposit Token
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount);
+        result = await transaction.wait();
+
+
+        // Now withdraw tokens 
+        transaction = await exchange.connect(user1).withdrawToken(token1.address, amount)
+        result = await transaction.wait();
+
+      });
+
+      it("withdraws token funds", async () => {
+        expect(await token1.balanceOf(exchange.address)).to.equal(0);
+        expect(await exchange.tokens(token1.address, user1.address)).to.equal(0);
+        expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(0);
+      });
+
+      it("emits a withdraw Event", async () => {
+        const event = result.events[1]; // 2 events are emitted
+        expect(event.event).to.equal("Withdraw");
+
+        const args = event.args;
+        expect(args.token).to.equal(token1.address);
+        expect(args.user).to.equal(user1.address);
+        expect(args.amount).to.equal(amount);
+        expect(args.balance).to.equal(0);
+      });
+    });
+    describe("Failure", () => {
+      it("fails for insufficient balances", async () => {
+        // Attempting to withdraw tokens without depositing
         await expect(exchange.connect(user1).depositToken(token1.address, amount)).to.be.reverted
       });
     });
+  });
+
+  describe("Checking balances", () => {
+    let transaction, result;
+    let amount = tokens(1);
+
+      beforeEach(async () => {
+        // Approve Token
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+
+        // Deposit Token
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount);
+        result = await transaction.wait();
+      });
+
+      it("returns user balance", async () => {
+        expect(await token1.balanceOf(exchange.address)).to.equal(amount);
+      });
   });
 });
