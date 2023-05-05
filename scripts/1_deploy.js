69fa6ac38702766d0e379126d2088084d608cf6e
Sebastien Zasada <sezasada@icloud.com>
2023-05-05 12:27:39 -0500

    Updated the deploy script, removed the sample script, added the seed exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,13 +1,34 @@
-const { ethers } = require('hardhat')
+const { ethers } = require("hardhat");
 
 async function main() {
-    // fetch contract to deploy 
-    const Token = await ethers.getContractFactory("Token")
+  console.log(`Preparing deployment... \n`);
+  // fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
 
-    // deploy contract
-    const token = await Token.deploy()
-    await token.deployed()
-    console.log(`Token Deployed to: ${token.address}`)
+  // Fetch accounts
+  const accounts = await ethers.getSigners();
+
+  console.log(
+    `Accounts fetched: \n${accounts[0].address}\n${accounts[1].address}\n`
+  );
+
+  // deploy contract
+  const sebz = await Token.deploy("Sebastien's Token", "SEBZ", '1000000');
+  await sebz.deployed();
+  console.log(`SEBZ Deployed to: ${sebz.address}`);
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000');
+  await mETH.deployed();
+  console.log(`mETH Deployed to: ${mETH.address}`);
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000');
+  await mDAI.deployed();
+  console.log(`mDAI Deployed to: ${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
 }
 
 main().catch((error) => {
