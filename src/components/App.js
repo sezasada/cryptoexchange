a5a588230580f289b86d1fcb1e7e69b089d07694
Sebastien Zasada <sezasada@icloud.com>
2023-05-18 10:47:24 -0500

    initialize new branch

---
+++ src/components/App.js
@@ -0,0 +1,74 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import config from '../config.json';
+
+import {
+  loadProvider,
+  loadNetwork,
+  loadAccount,
+  loadTokens,
+  loadExchange
+} from '../store/interactions';
+
+function App() {
+  const dispatch = useDispatch()
+
+  const loadBlockchainData = async () => {
+    // Connect Ethers to blockchain
+    const provider = loadProvider(dispatch)
+
+    // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
+    const chainId = await loadNetwork(provider, dispatch)
+
+    // Fetch current account & balance from Metamask
+    await loadAccount(provider, dispatch)
+
+    // Load token smart contracts
+    const SEBZ = config[chainId].SEBZ
+    const mETH = config[chainId].mETH
+    await loadTokens(provider, [SEBZ.address, mETH.address], dispatch)
+
+    // Load exchange smart contract
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  })
+
+  return (
+    <div>
+
+      {/* Navbar */}
+
+      <main className='exchange grid'>
+        <section className='exchange__section--left grid'>
+
+          {/* Markets */}
+
+          {/* Balance */}
+
+          {/* Order */}
+
+        </section>
+        <section className='exchange__section--right grid'>
+
+          {/* PriceChart */}
+
+          {/* Transactions */}
+
+          {/* Trades */}
+
+          {/* OrderBook */}
+
+        </section>
+      </main>
+
+      {/* Alert */}
+
+    </div>
+  );
+}
+
+export default App;
