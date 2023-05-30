980ba5dfa5b6e07af476d41a3c643cb79234aead
Sebastien Zasada <sezasada@icloud.com>
2023-05-30 10:15:48 -0500

    created order book. any buy/sell order are being displayed properly.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,193 +1,220 @@
 export const provider = (state = {}, action) => {
   switch (action.type) {
-    case 'PROVIDER_LOADED':
+    case "PROVIDER_LOADED":
       return {
         ...state,
-        connection: action.connection
-      }
-    case 'NETWORK_LOADED':
+        connection: action.connection,
+      };
+    case "NETWORK_LOADED":
       return {
         ...state,
-        chainId: action.chainId
-      }
-    case 'ACCOUNT_LOADED':
+        chainId: action.chainId,
+      };
+    case "ACCOUNT_LOADED":
       return {
         ...state,
-        account: action.account
-      }
-    case 'ETHER_BALANCE_LOADED':
+        account: action.account,
+      };
+    case "ETHER_BALANCE_LOADED":
       return {
         ...state,
-        balance: action.balance
-      }
+        balance: action.balance,
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
 const DEFAULT_TOKENS_STATE = {
   loaded: false,
   contracts: [],
-  symbols: []
-}
+  symbols: [],
+};
 
 export const tokens = (state = DEFAULT_TOKENS_STATE, action) => {
   switch (action.type) {
-    case 'TOKEN_1_LOADED':
+    case "TOKEN_1_LOADED":
       return {
         ...state,
         loaded: true,
         contracts: [action.token],
-        symbols: [action.symbol]
-      }
-    case 'TOKEN_1_BALANCE_LOADED':
+        symbols: [action.symbol],
+      };
+    case "TOKEN_1_BALANCE_LOADED":
       return {
         ...state,
-        balances: [action.balance]
-      }
+        balances: [action.balance],
+      };
 
-    case 'TOKEN_2_LOADED':
+    case "TOKEN_2_LOADED":
       return {
         ...state,
         loaded: true,
         contracts: [...state.contracts, action.token],
-        symbols: [...state.symbols, action.symbol]
-      }
+        symbols: [...state.symbols, action.symbol],
+      };
 
-    case 'TOKEN_2_BALANCE_LOADED':
+    case "TOKEN_2_BALANCE_LOADED":
       return {
         ...state,
-        balances: [...state.balances, action.balance]
-      }
+        balances: [...state.balances, action.balance],
+      };
 
-      default:
-        return state
+    default:
+      return state;
   }
-}
+};
 
 const DEFAULT_EXCHANGE_STATE = {
   loaded: false,
   contract: {},
   transaction: {
-    isSuccessful: false
+    isSuccessful: false,
   },
   allOrders: {
     loaded: false,
-    data: []
+    data: [],
   },
-  events: []
-}
+  events: [],
+};
 
 export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
-  let index, data
+  let index, data;
 
   switch (action.type) {
-    case 'EXCHANGE_LOADED':
+    case "EXCHANGE_LOADED":
       return {
         ...state,
         loaded: true,
-        contract: action.exchange
-      }
+        contract: action.exchange,
+      };
 
-    // ------------------------------------------------------------------------------
+    // ORDERS LOADED (CANCELLED, FILLED & ALL)
+
+    case "CANCELLED_ORDERS_LOADED":
+      return {
+        ...state,
+        cancelledOrders: {
+          loaded: true,
+          data: action.cancelledOrders,
+        },
+      };
+
+    case "FILLED_ORDERS_LOADED":
+      return {
+        ...state,
+        filledOrders: {
+          loaded: true,
+          data: action.filledOrders,
+        },
+      };
+
+    case "ALL_ORDERS_LOADED":
+      return {
+        ...state,
+        allOrders: {
+          loaded: true,
+          data: action.allOrders,
+        },
+      };
+
     // BALANCE CASES
-    case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
+    case "EXCHANGE_TOKEN_1_BALANCE_LOADED":
       return {
         ...state,
-        balances: [action.balance]
-      }
-    case 'EXCHANGE_TOKEN_2_BALANCE_LOADED':
+        balances: [action.balance],
+      };
+    case "EXCHANGE_TOKEN_2_BALANCE_LOADED":
       return {
         ...state,
-        balances: [...state.balances, action.balance]
-      }
+        balances: [...state.balances, action.balance],
+      };
 
-    // ------------------------------------------------------------------------------
     // TRANSFER CASES (DEPOSIT & WITHDRAWS)
-    case 'TRANSFER_REQUEST':
+    case "TRANSFER_REQUEST":
       return {
         ...state,
         trasnsaction: {
-          transactionType: 'Transfer',
+          transactionType: "Transfer",
           isPending: true,
-          isSuccessful: false
+          isSuccessful: false,
         },
-        transferInProgress: true
-      }
-    case 'TRANSFER_SUCCESS':
+        transferInProgress: true,
+      };
+    case "TRANSFER_SUCCESS":
       return {
         ...state,
         transaction: {
-          transactionType: 'Transfer',
+          transactionType: "Transfer",
           isPending: false,
-          isSuccessful: true
+          isSuccessful: true,
         },
         transferInProgress: false,
-        events: [action.event, ...state.events]
-      }
-    case 'TRANSFER_FAIL':
+        events: [action.event, ...state.events],
+      };
+    case "TRANSFER_FAIL":
       return {
         ...state,
         transaction: {
-          transactionType: 'Transfer',
+          transactionType: "Transfer",
           isPending: false,
           isSuccessful: false,
-          isError: true
-
+          isError: true,
         },
-        transferInProgress: false
-      }
+        transferInProgress: false,
+      };
 
-    // ------------------------------------------------------------------------------
     // MAKING ORDERS CASES
 
-    case 'NEW_ORDER_REQUEST':
+    case "NEW_ORDER_REQUEST":
       return {
         ...state,
         transaction: {
-          transactionType: 'New Order',
+          transactionType: "New Order",
           isPending: true,
-          isSuccessful: false
+          isSuccessful: false,
         },
-      }
+      };
 
-    case 'NEW_ORDER_SUCCESS':
+    case "NEW_ORDER_SUCCESS":
       // Prevent duplicate orders
-      index = state.allOrders.data.findIndex(order => order.id === action.order.id)
+      index = state.allOrders.data.findIndex(
+        (order) => order.id.toString() === action.order.id.toString()
+      );
 
-      if(index === -1) {
-        data = [...state.allOrders.data, action.order]
+      if (index === -1) {
+        data = [...state.allOrders.data, action.order];
       } else {
-        data = state.allOrders.data
+        data = state.allOrders.data;
       }
 
       return {
         ...state,
         allOrders: {
           ...state.allOrders,
-          data
+          data,
         },
         transaction: {
-          transactionType: 'New Order',
+          transactionType: "New Order",
           isPending: false,
-          isSuccessful: true
+          isSuccessful: true,
         },
-        events: [action.event, ...state.events]
-      }
+        events: [action.event, ...state.events],
+      };
 
-    case 'NEW_ORDER_FAIL':
+    case "NEW_ORDER_FAIL":
       return {
         ...state,
         transaction: {
-          transactionType: 'New Order',
+          transactionType: "New Order",
           isPending: false,
           isSuccessful: false,
-          isError: true
+          isError: true,
         },
-      }
+      };
 
-      default:
-        return state
+    default:
+      return state;
   }
-}
+};
